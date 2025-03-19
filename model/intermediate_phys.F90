e0ccf718b0ac9014afc96048e3e08a3996299a89
Linjiong Zhou <Linjiong.Zhou@noaa.gov>
2025-03-19 08:56:52 -0400

    Apply every integrated physics to all model layers 1) to avoid model crash for P3 mp, and 2) to ensure integrated physics are activated at higher model layers.

--- model/intermediate_phys.F90
+++ model/intermediate_phys.F90
@@ -196,19 +196,16 @@ subroutine intermediate_phys (is, ie, js, je, isd, ied, jsd, jed, km, npx, npy, 
 
     rrg = - rdgas / grav
 
-    if (mp_flag .eq. 7) then
-        kmp = 1
-    else
-        ! time saving trick
-        if (last_step) then
-            kmp = 1
-        else
-            do k = 1, km
-                kmp = k
-                if (pfull (k) .gt. 50.E2) exit
-            enddo
-        endif
-    endif
+    kmp = 1
+    ! time saving trick
+    ! if (last_step) then
+    !     kmp = 1
+    ! else
+    !     do k = 1, km
+    !         kmp = k
+    !         if (pfull (k) .gt. 50.E2) exit
+    !     enddo
+    ! endif
 
     ! decide which tracer needs adjustment
     if (.not. allocated (conv_vmr_mmr)) allocate (conv_vmr_mmr (nq))
